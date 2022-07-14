a4257c2dfea7588142b07543476039659e60667e
ashari0831 <ashari0831@gmail.com>
2022-07-14 23:28:27 +0430

    name of a mail file changed

--- app/Mail/SendTicket.php
+++ app/Mail/SendTicket.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class SendConfirmEmail extends Mailable
+class SendTicket extends Mailable
 {
     use Queueable, SerializesModels;
 
