29f1772c185893a465ba11f90f8eba9288c40092
ashari0831 <ashari0831@gmail.com>
2022-07-14 23:40:30 +0430

    emails are ready for queue

--- app/Mail/SendTicket.php
+++ app/Mail/SendTicket.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class SendConfirmEmail extends Mailable
+class SendTicket extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -28,6 +28,6 @@ class SendConfirmEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.ticket');
     }
 }
